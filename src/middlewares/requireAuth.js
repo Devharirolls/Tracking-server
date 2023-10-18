553b047323b61f04dc0460477bc182d8171ca662
Hariharan <138101854+Devharirolls@users.noreply.github.com>
2023-10-18 18:14:48 +0530

    Update requireAuth.js

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -4,7 +4,7 @@ const User = mongoose.model('User');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  // authorization === 'Bearer laksjdflaksdjasdfklj'
+ 
 
   if (!authorization) {
     return res.status(401).send({ error: 'You must be logged in.' });
